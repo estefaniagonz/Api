f058f5d8f1bece14f1332485eab85ae9b276494b
Estefania <estefanianietogonzalez33@gmail.com>
2024-03-01 18:44:13 +0100

    Creacion de api con informacion de estudiantes

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const students = [
+    { id: 1, name: "Hanna", age: 13, grado: 'Septimo' },
+    { id: 2, name: "Jorge", age: 18, grado: 'Once'},
+    { id: 3, name: "luis", age: 8, grado: 'Quinto' },
+    { id: 4, name: "Sofia", age: 15, grado: 'Decimo' },
+    { id: 5, name: "Juan", age: 15, grado: 'Decimo' },
+];
+
+app.get('/', (req, res) => {
+    res.send('Node JS api');
+});
+
+app.get('/api/students', (req, res) => {
+    res.send(students);
+});
+
+app.get('/api/students/:id', (req, res) => {
+    const student = students.find(c => c.id === parseInt(req.params.id));
+    if (!student)
+        return res.status(404).send("Estudiante no encontrado");
+    else res.send(student);
+});
+
+app.post('/api/students', (req, res) => {
+    const student = {
+        id: students.length + 1,
+        name: req.body.name,
+        age: parseInt(req.body.age),
+        enroll: (req.body.enroll === 'true')
+    };
+
+    students.push(student);
+    res.send(student);
+});
+
+app.delete('/api/students/:id', (req, res) => {
+    const student = students.find(c => c.id === parseInt(req.params.id));
+    if (!student) return res.status(404).send('Estudiante no encontrado');
+
+    const index = students.indexOf(student);
+    students.splice(index, 1);
+    res.send(student);
+});
+
+const port = process.env.PORT || 80;
+app.listen(port, () => console.log(`Escuchando en puerto ${port}`));
